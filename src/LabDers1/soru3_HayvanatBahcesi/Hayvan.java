cdb9485461317efd3c85865fe19081cb72c7c477
ahmedfaruker <152505906+ahmedfaruker@users.noreply.github.com>
2024-03-27 16:09:29 +0300

    reset

---
+++ src/LabDers1/soru3_HayvanatBahcesi/Hayvan.java
@@ -0,0 +1,6 @@
+package LabDers1.soru3_HayvanatBahcesi;
+
+public class Hayvan {
+    String ses;
+    String isim;
+}
---
+++ src/LabDers1/soru3_HayvanatBahcesi/Kopek.java
@@ -0,0 +1,13 @@
+package LabDers1.soru3_HayvanatBahcesi;
+
+public class Kopek extends Hayvan{
+    String tuyRengi;
+    public Kopek(String isim,String tuyRengi){
+        this.isim=isim;
+        this.ses="havhav";
+        this.tuyRengi=tuyRengi;
+    }
+    public void kopekOzellik(){
+        System.out.println("Kopegin ismi:"+isim+" ,Kopegin sesi:"+ses+" ,Kopegin tuy rengi:"+tuyRengi);
+    }
+}
